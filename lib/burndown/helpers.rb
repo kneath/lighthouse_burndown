f28f05f3a21d9252aeb4712d9cfbcb254c273abc
Kyle Neath <kneath@gmail.com>
2009-04-22 19:51:09 -0700

    Make sure the last date is always shown

--- lib/burndown/helpers.rb
+++ lib/burndown/helpers.rb
@@ -20,10 +20,16 @@ module Burndown
     def segmented_range(start_date, end_date, options = {}, &block)
       options[:segments] ||= 30
       options[:segements] = options[:segments] - 1
+      
+      # Find the correct step size
+      range = start_date..end_date
+      step_size = [range.to_a.size/options[:segments], 1].max
+      
+      # We need to always include the last one, which may mean removing items from the beginning to satisfy...
+      start_date = start_date + range.to_a.size % step_size + (step_size - 1)
       range = start_date..end_date
       
-      step_size = [range.to_a.size/options[:segments], 1].max
-      (start_date..end_date).step(step_size, &block)
+      range.step(step_size, &block)
     end
   end
 end
