8f9cd0fad772456d82d78bd2405f4920b19f9898
Kyle Neath <kneath@gmail.com>
2009-04-27 18:28:26 -0700

    Use active_since, not active

--- lib/burndown/project.rb
+++ lib/burndown/project.rb
@@ -2,18 +2,22 @@ module Burndown
   class Project
     include DataMapper::Resource
     
-    property :id,       Serial
-    property :name,     String,   :nullable => false
-    property :active,   Boolean,  :default => false
+    property :id,             Serial
+    property :name,           String,   :nullable => false
+    property :active_since,   DateTime
     
     # Calls the api to get projects for the token
     def self.for_token(token)
       result = Lighthouse.get_projects(token.account, token.token)
       arr = []
       result["projects"].each do |project|
-        arr.push(Project.new(:name => project["name"], :active => false))
+        arr.push(Project.new(:name => project["name"]))
       end
       arr
+    end
+    
+    def active?
+      active_since && active_since < Time.now.utc
     end
   end
 end
