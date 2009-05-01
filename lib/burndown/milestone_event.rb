8bce06dcd905f6494a47d769c427e0c59b5ec60a
Kyle Neath <kneath@gmail.com>
2009-05-01 02:37:12 -0700

    Add all kinds of logic fixes

--- lib/burndown/milestone_event.rb
+++ lib/burndown/milestone_event.rb
@@ -9,7 +9,11 @@ module Burndown
     belongs_to :milestone
     
     def prev_record
-      @prev_record ||= (self.class.first(:created_on.lt => self.created_on) || MilestoneEvent.new)
+      @prev_record ||= (self.class.first(:created_on.lt => self.created_on, :order => [:created_on.desc]) || MilestoneEvent.new)
+    end
+    
+    def num_tickets_open
+      @@num_tickets_open = self.open_tickets.split(',').size
     end
     
     def tickets_opened
