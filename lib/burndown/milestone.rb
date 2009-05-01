15ab64f404ad836e52aa7222c62318aa9d2c78c9
Kyle Neath <kneath@gmail.com>
2009-05-01 00:24:40 -0700

    Track when the milestone was added to burndown

--- lib/burndown/milestone.rb
+++ lib/burndown/milestone.rb
@@ -5,6 +5,7 @@ module Burndown
     property :id,             Serial
     property :remote_id,      Integer,  :nullable => false
     property :name,           String
+    property :activated_at,   DateTime
     
     belongs_to :project
     
