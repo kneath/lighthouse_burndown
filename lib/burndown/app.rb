6b381bfe737d4f89fd99e6ae26006bb3cd511a5e
Kyle Neath <kneath@gmail.com>
2009-05-02 17:18:12 -0700

    Really fix sorting

--- lib/burndown/app.rb
+++ lib/burndown/app.rb
@@ -14,7 +14,7 @@ module Burndown
     
     get "/project/:id" do
       @project = Project.get(params[:id])
-      @milestones = @project.milestones.all(:order => [:due_on.asc])
+      @milestones = @project.milestones.all(:order => [:due_on.asc]).sort { |x, y| x.due_on.nil? ? 1 : x.due_on <=> y.due_on }
       show :project
     end
     
